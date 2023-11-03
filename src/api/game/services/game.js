92b455ca8d633c5c5b3a5ceb6d068bf27b69972d
Darlley <systembrtt@gmail.com>
2023-11-03 13:42:28 -0400

    fetch

--- src/api/game/services/game.js
+++ src/api/game/services/game.js
@@ -4,17 +4,20 @@
  * game service
  */
 
-
+const fetch = require('node-fetch-commonjs');
 const { createCoreService } = require('@strapi/strapi').factories;
 
 module.exports = createCoreService('api::game.game', () => ({
     async populate(params) {
-        console.log(params)
-        const cat = await strapi.service("api::category.category").find({
-            filters: {
-                name: params.category
-            }
-        })
-        console.log(cat)
-    }
+        const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game?sort=rating`;
+    
+        // Usando fetch em vez de axios
+        fetch(gogApiUrl)
+          .then(response => response.json()) // Convertendo a resposta em JSON
+          .then(data => {
+            const products = data.products; // Acessando a propriedade products do objeto data
+            console.log(products[1]); // Mostrando o segundo produto no console
+          })
+          .catch(error => console.error(error)); // Mostrando o erro no console se houver
+      },
 }));
