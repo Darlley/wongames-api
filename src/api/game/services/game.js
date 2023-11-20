707be7db27930cd75737928ebafa0bf1edd797fc
Darlley <systembrtt@gmail.com>
2023-11-20 08:22:40 -0400

    update

--- src/api/game/services/game.js
+++ src/api/game/services/game.js
@@ -3,9 +3,9 @@
 /**
 
 game service */
-const fetch = require('node-fetch-commonjs'); 
-const {JSDOM} = require('jsdom'); 
-const slugify = require('slugify'); 
+const fetch = require('node-fetch-commonjs');
+const { JSDOM } = require('jsdom');
+const slugify = require('slugify');
 const { createCoreService } = require('@strapi/strapi').factories;
 
 const gameService = "api::game.game";
@@ -14,43 +14,43 @@ const developerService = "api::developer.developer";
 const categoryService = "api::category.category";
 const platformService = "api::platform.platform";
 
-async function getGameInfo(slug){ 
-  const gogSlug = slug.replaceAll('-', '_').toLowerCase();
-  const response = await fetch(`https://www.gog.com/game/${gogSlug}`); 
-  const data = await response.text(); 
+async function getGameInfo(slug) {
+  const gogSlug = slug.replace('-', '_').toLowerCase();
+  const response = await fetch(`https://www.gog.com/game/${gogSlug}`);
+  const data = await response.text();
   const dom = new JSDOM(data);
 
   const $row_description = dom.window.document.querySelector(".description")
   const description = $row_description.innerHTML
-  const short_description = $row_description.textContent.trim().slice(0,160);
+  const short_description = $row_description.textContent.trim().slice(0, 160);
   const ratingElement = dom.window.document.querySelector('.age-restrictions__icon use')
-  
+
   return {
     description: description ? description : '',
     short_description: short_description ? short_description : '',
-    rating: ratingElement ? 
-      ratingElement.getAttribute("xlink:href")
-      .replace(/_/g, "") 
-      .replace("#", "") 
-    : 'BR0'
+    rating: ratingElement ?
+      ratingElement.getAttributeNS("xlink:href", "href")
+        .replace(/_/g, "")
+        .replace("#", "")
+      : 'BR0'
   }
 }
 
-async function getByName(name, entityService){
+async function getByName(name, entityService) {
   try {
-    const item = await strapi.service(`api::${entityService}.${entityService}`).find({ 
-      populate: name,
-    })
+    const item = await strapi.service(`api::${entityService}.${entityService}`).find({
+      filters: { name },
+    });
     return item.results.length > 0 ? item.results[0] : null;
-  }catch(error){
+  } catch (error) {
     console.log("getByName:", error);
   }
 }
 
-async function createByName(name, entityService){
-  if(name && entityService){
+async function createByName(name, entityService) {
+  if (name && entityService) {
     const item = await getByName(name, entityService)
-    if(!item){
+    if (!item) {
       await strapi.service(`api::${entityService}.${entityService}`).create({
         data: {
           name: name,
@@ -64,27 +64,27 @@ async function createByName(name, entityService){
   }
 }
 
-async function createManyToManyData(products){
+async function createManyToManyData(products) {
   const developerSet = new Set();
   const publisherSet = new Set();
   const categoriesSet = new Set();
   const platformsSet = new Set();
 
   products.forEach(product => {
-    const {developer, publisher, genres, supportedOperatingSystems} = product
+    const { developer, publisher, genres, supportedOperatingSystems } = product
 
     genres?.forEach((name) => {
       categoriesSet.add(name)
     })
-    supportedOperatingSystems?.forEach(({item}) => {
+    supportedOperatingSystems?.forEach(({ item }) => {
       platformsSet.add(item)
     })
 
-    if(developer){
+    if (developer) {
       developerSet.add(developer)
     }
 
-    if(publisher){
+    if (publisher) {
       publisherSet?.add(publisher)
     }
   });
@@ -99,13 +99,24 @@ async function createManyToManyData(products){
   ])
 }
 
-async function createGames(products){
-  
+async function createGames(products) {
   await Promise.all(
-    products.map(async (product) => {
+    products.forEach(async (product) => {
       const item = await getByName(product.title, 'game');
 
-      if (!item) {
+      if (
+        !item && 
+        product.title &&
+        product.slug &&
+        product.price.amount && 
+        product.globalReleaseDate &&
+        (product.genres && product.genres.length > 0) &&
+        (product.supportedOperatingSystems && product.supportedOperatingSystems.length > 0) &&
+        product.developer &&
+        product.publisher
+        ) {
+        console.info(`Creating: ${product.title}...`);
+
         const game = await strapi.service(gameService).create({
           data: {
             name: product.title,
@@ -113,26 +124,27 @@ async function createGames(products){
             price: Number(product.price.amount),
             release_date: new Date(product.globalReleaseDate),
             categories: await Promise.all(
-              product.genres.map((category) => getByName(category, 'category'))
+              (product.genres && product.genres.length > 0) && product.genres.map((category) => getByName(category, 'category'))
             ),
             platforms: await Promise.all(
-              product.supportedOperatingSystems.map((platform) => getByName(platform, 'platform'))
+              (product.supportedOperatingSystems && product.supportedOperatingSystems.length > 0) && product.supportedOperatingSystems.map((platform) => getByName(platform, 'platform'))
             ),
-            developers: await getByName(product.developer, 'developer'),
-            publisher: await getByName(product.publisher, 'publisher'),
-
+            developers: product.developer && await getByName(product.developer, 'developer'),
+            publisher: product.publisher && await getByName(product.publisher, 'publisher'),
+  
             ...(await getGameInfo(product.slug)),
-
+  
             publishedAt: new Date(),
           }
         })
+
         return game
       }
     })
   )
 }
 
-module.exports = createCoreService('api::game.game', () => ({ 
+module.exports = createCoreService('api::game.game', () => ({
   async populate(params) {
     const gogApiUrl = 'https://www.gog.com/games/ajax/filtered?mediaType=game?sort=rating';
 
