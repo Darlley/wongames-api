c3bf4c0b35ba2dfeff2fdbbbf061fb667c8d7049
Darlley <systembrtt@gmail.com>
2025-02-13 22:04:27 -0400

    feat: createGames without images

--- src/api/game/services/game.ts
+++ src/api/game/services/game.ts
@@ -7,6 +7,12 @@ import axios from 'axios';
 import { JSDOM } from 'jsdom';
 import slugify from 'slugify';
 
+const gameService = "api::game.game";
+const publisherService = "api::publisher.publisher";
+const developerService = "api::developer.developer";
+const categoryService = "api::category.category";
+const platformService = "api::platform.platform";
+
 async function getGameInfo(slug: string) {
   try {
     const gogSlug = slug.replace('-', '_').toLowerCase();
@@ -105,7 +111,7 @@ async function getGameInfo(slug: string) {
 
 async function getByName(name, entityService) {
   const item = await strapi
-    .service(`api::${entityService}.${entityService}`)
+    .service(entityService)
     .find({
       filters: { name },
     });
@@ -117,7 +123,7 @@ async function create(name, entityService) {
   const item = await getByName(name, entityService);
 
   if (!item) {
-    await strapi.service(`api::${entityService}.${entityService}`).create({
+    await strapi.service(entityService).create({
       data: {
         name,
         slug: slugify(name, { strict: true, lower: true }),
@@ -161,14 +167,57 @@ async function createManyToManyData(products) {
     Array.from(set).map((name) => create(name, entityService));
 
   return Promise.all([
-    ...createCall(developersSet, 'developer'),
-    ...createCall(publishersSet, 'publisher'),
-    ...createCall(categoriesSet, 'category'),
-    ...createCall(platformsSet, 'platform'),
+    ...createCall(developersSet, developerService),
+    ...createCall(publishersSet, publisherService),
+    ...createCall(categoriesSet, categoryService),
+    ...createCall(platformsSet, platformService),
   ]);
 }
 
-export default factories.createCoreService('api::game.game', () => ({
+async function createGames(products) {
+  await Promise.all(
+    products.map(async (product) => {
+      const item = await getByName(product.title, gameService);
+
+      if (!item) {
+        console.info(`Creating: ${product.title}...`);
+
+        const game = await strapi.service(gameService).create({
+          data: {
+            name: product.title,
+            slug: product.slug,
+            price: product.price.finalMoney.amount,
+            release_date: new Date(product.releaseDate),
+            categories: await Promise.all(
+              product.genres.map(({ name }) => getByName(name, categoryService))
+            ),
+            platforms: await Promise.all(
+              product.operatingSystems.map((name) =>
+                getByName(name, platformService)
+              )
+            ),
+            developers: await Promise.all(
+              product.developers.map((name) =>
+                getByName(name, developerService)
+              )
+            ),
+            publisher: await Promise.all(
+              product.publishers.map((name) =>
+                getByName(name, publisherService)
+              )
+            ),
+            ...(await getGameInfo(product.slug)),
+            publishedAt: new Date(),
+          },
+        });
+
+        return game;
+      }
+    })
+  );
+}
+
+export default factories.createCoreService(gameService, () => ({
   async populate(params) {
     try {
       const gogApiUrl =
@@ -180,10 +229,8 @@ export default factories.createCoreService('api::game.game', () => ({
         data: DataType;
       } = await axios.get(gogApiUrl);
 
-      console.log(products[1]);
-
-      // console.log(await getGameInfo(products[0].slug));
       await createManyToManyData([products[0], products[1]])
+      await createGames([products[0], products[1]])
 
     } catch (error) {
       console.error('Erro ao popular jogos:', error);
