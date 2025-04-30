ecd7bb3b7f53d4e6886ec8854ed945530ac5f564
Darlley <systembrtt@gmail.com>
2025-04-30 14:30:43 -0400

    aula 276

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,41 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface PageButton extends Struct.ComponentSchema {
+  collectionName: 'components_page_buttons';
+  info: {
+    displayName: 'button';
+    icon: 'link';
+  };
+  attributes: {
+    label: Schema.Attribute.String & Schema.Attribute.Required;
+    link: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface PageRibbon extends Struct.ComponentSchema {
+  collectionName: 'components_page_ribbons';
+  info: {
+    displayName: 'ribbon';
+    icon: 'pin';
+  };
+  attributes: {
+    color: Schema.Attribute.Enumeration<['primary', 'secondary']> &
+      Schema.Attribute.DefaultTo<'primary'>;
+    size: Schema.Attribute.Enumeration<['small', 'normal']> &
+      Schema.Attribute.DefaultTo<'normal'>;
+    text: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 30;
+      }>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'page.button': PageButton;
+      'page.ribbon': PageRibbon;
+    }
+  }
+}
