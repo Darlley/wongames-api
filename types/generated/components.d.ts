d0265c883a0ef6983063bbd46e6d038bacd0e0b1
Darlley <systembrtt@gmail.com>
2025-05-16 18:48:25 -0400

    aula 282

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -12,6 +12,37 @@ export interface PageButton extends Struct.ComponentSchema {
   };
 }
 
+export interface PageHighlight extends Struct.ComponentSchema {
+  collectionName: 'components_page_highlights';
+  info: {
+    displayName: 'highlight';
+    icon: 'star';
+  };
+  attributes: {
+    alignment: Schema.Attribute.Enumeration<['right', 'left']> &
+      Schema.Attribute.DefaultTo<'right'>;
+    background: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
+    buttonLabel: Schema.Attribute.String & Schema.Attribute.Required;
+    buttonLink: Schema.Attribute.Text & Schema.Attribute.Required;
+    floatImage: Schema.Attribute.Media<'images'>;
+    subtitle: Schema.Attribute.Text & Schema.Attribute.Required;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface PagePopularGames extends Struct.ComponentSchema {
+  collectionName: 'components_page_popular_games';
+  info: {
+    displayName: 'popularGames';
+    icon: 'cursor';
+  };
+  attributes: {
+    games: Schema.Attribute.Relation<'oneToMany', 'api::game.game'>;
+    highlight: Schema.Attribute.Component<'page.highlight', false>;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
 export interface PageRibbon extends Struct.ComponentSchema {
   collectionName: 'components_page_ribbons';
   info: {
@@ -31,11 +62,26 @@ export interface PageRibbon extends Struct.ComponentSchema {
   };
 }
 
+export interface PageSection extends Struct.ComponentSchema {
+  collectionName: 'components_page_sections';
+  info: {
+    displayName: 'section';
+    icon: 'stack';
+  };
+  attributes: {
+    highlight: Schema.Attribute.Component<'page.highlight', false>;
+    title: Schema.Attribute.String;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
       'page.button': PageButton;
+      'page.highlight': PageHighlight;
+      'page.popular-games': PagePopularGames;
       'page.ribbon': PageRibbon;
+      'page.section': PageSection;
     }
   }
 }
